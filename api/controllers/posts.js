e971bd06a9f2731c2b552b0884db2f73690c212f
Marya <ms930652210@googlemail.com>
2024-08-01 11:25:03 +0100

    Updated Post schema and controllers

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -8,7 +8,22 @@ const getAllPosts = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  const post = new Post(req.body);
+  const {post_title, message, post_author, is_private} = req.body;
+  // const post_title = req.body.title;
+  // const message = req.body.message;
+  // const author = req.user_id
+  // const date = new Date.now();
+  // const is_private = req.body.is_private
+  const date_created = new Date().toLocaleString("en-GB")
+
+  const post = new Post({
+    post_title,
+    message,
+    post_author,
+    date_created,
+    is_private
+  });
+
   post.save();
 
   const newToken = generateToken(req.user_id);
--- api/models/post.js
+++ api/models/post.js
@@ -1,9 +1,14 @@
+const User = require('./user'); 
 const mongoose = require("mongoose");
 
 // A Schema defines the "shape" of entries in a collection. This is similar to
 // defining the columns of an SQL Database.
 const PostSchema = new mongoose.Schema({
-  message: String,
+  title: {type: String, required: true},
+  message: {type : String, required: true,  maxlength: 300},
+  post_author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  date_created : {type: Date, required: true},
+  is_private : {type: Boolean, required: true} 
 });
 
 // We use the Schema to create the Post model. Models are classes which we can
@@ -12,7 +17,7 @@ const Post = mongoose.model("Post", PostSchema);
 
 // These lines will create a test post every time the server starts.
 // You can delete this once you are creating your own posts.
-const dateTimeString = new Date().toLocaleString("en-GB");
-new Post({ message: `Test message, created at ${dateTimeString}` }).save();
+// const dateTimeString = new Date().toLocaleString("en-GB");
+// new Post({ message: `Test message, created at ${dateTimeString}` }).save();
 
 module.exports = Post;
