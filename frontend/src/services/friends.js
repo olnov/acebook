33992e59ea256f34422651ebed8723c83933b756
chobs95 <charlieroberts201@hotmail.co.uk>
2024-08-01 16:18:06 +0100

    added the friends services - no front end done yet

--- frontend/src/services/friends.js
+++ frontend/src/services/friends.js
@@ -1,1 +1,40 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+
+// Function to get a user's friends
+export const getUserFriends = async (userId, token) => {
+    const requestOptions = {
+        method: "GET",
+        headers: {
+    Authorization: `Bearer ${token}`,
+    },
+};
+
+    const response = await fetch(`${BACKEND_URL}/users/${userId}/friends`, requestOptions);
+
+    if (response.status !== 200) {
+        throw new Error("Unable to fetch user's friends");
+    }
+
+    const data = await response.json();
+    return data;
+};
+
+// Function to add or remove a friend
+export const addRemoveFriend = async (userId, friendId, token) => {
+    const requestOptions = {
+        method: "PATCH",
+        headers: {
+    Authorization: `Bearer ${token}`,
+    },
+};
+
+    const response = await fetch(`${BACKEND_URL}/users/${userId}/${friendId}`, requestOptions);
+
+    if (response.status !== 200) {
+        throw new Error("Unable to add/remove friend");
+}
+
+    const data = await response.json();
+    return data;
+};
