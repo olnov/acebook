a987aa4c22db83a978297aebc67f91469d5fcb2e
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-06 10:22:56 +0100

    Freindlist changes

--- frontend/src/components/FriendList/FriendList.jsx
+++ frontend/src/components/FriendList/FriendList.jsx
@@ -1,11 +1,12 @@
+// FriendList.js
 import React from "react";
 import { FriendListFrame } from "../FriendListFrame";
 import "./style.css";
 
-export const FriendList = ({ className }) => {
+export const FriendList = ({ friends, className }) => {
   return (
     <div className={`friend-list ${className}`}>
-      <FriendListFrame className="friend-list-frame-instance" />
+      <FriendListFrame className="friend-list-frame-instance" friends={friends} />
     </div>
   );
 };
