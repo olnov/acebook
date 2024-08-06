b944d0172455ceabb87ca80c886ca15bb277c4a5
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-06 11:44:24 +0100

    Toggle dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import HomePage from './pages/Home/HomePage';
-// import FeedPage from './pages/Feed/FeedPage';
-// 
-// import LoginPage from './pages/Login/LoginPage';
-// import SignupPage from './pages/Signup/SignupPage';
-// import LandingPage from './pages/LandingPage/LandingPage';
-// import ResultsPage from './pages/Results/ResultsPage'; // import { SearchResultsPage } from "./pages/Search/SearchResultsPage";
-// import ProfilePage from './pages/Profile/ProfilePage';
-
-
+import React, { useState, useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/Home/HomePage";
@@ -19,11 +8,9 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import { FeedPage } from "./pages/Feed/FeedPage";
 import AboutUsPage from './pages/AboutUs/AboutUsPage';
 import { FriendsPage } from "./pages/Friends/FriendsPage";
-// import { MainPage } from "./pages/Main/MainPage"
 import { Profile } from "./pages/Profile/ProfilePage";
-// import { SearchResultsPage } from "./pages/Search/SearchResultsPage";
+import './index.css'; // Assuming your global styles are here
 
-// docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
   {
     path: "/Home",
@@ -53,83 +40,19 @@ const router = createBrowserRouter([
     path: "/friends", 
     element: <FriendsPage /> 
   },
-  // {
-  //   path: "/main",
-  //   element: <MainPage />
-  // },
   {
     path: "/profile/:userId",
     element: <Profile />
   },
-  // {
-  //   path: "/search-results",
-  //   element: <SearchResultsPage />
-  // },
 ]);
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/home" element={<HomePage />} />
-//         <Route path="/feed" element={<FeedPage />} />
-//         <Route path="/about-us" element={<AboutUsPage />} /> 
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/sign-up" element={<SignupPage />} />
-//         <Route path="/results" element={<ResultsPage />} />
-//         <Route path="/profile/:userId" element={<ProfilePage />} />
-//         <Route path="*" element={<div>404 Not Found</div>} />
-//       </Routes>
-//     </Router>
-
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// import "./App.css";
-// import { HomePage } from "./pages/Home/HomePage";
-// import { LoginPage } from "./pages/Login/LoginPage";
-// import { SignupPage } from "./pages/Signup/SignupPage";
-// import { FeedPage } from "./pages/Feed/FeedPage";
-
-// import { MainPage } from "./pages/Main/MainPage"
-// import { AuthContext } from "./context/AuthContext";
-
-
-// // docs: https://reactrouter.com/en/main/start/overview
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginPage />,
-//   },
-//   {
-//     path: "/signup",
-//     element: <SignupPage />,
-//   },
-//   {
-//     path: "/posts",
-//     element: <FeedPage />,
-//   },
-
-
-//   // { path: "/search-friends", 
-//   //   element: <SearchFriendsPage /> 
-//   // },
-//   {
-//     path: "/main",
-//     element: <MainPage />
-//   }
-// ]);
-
 const App = () => {
+
   return (
     <>
 
       <RouterProvider router={router} />
-
+    
     </>
   );
 };
