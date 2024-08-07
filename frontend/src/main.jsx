853ab02b354141e262a261e356309241b619877e
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-07 14:04:21 +0100

    Main change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { ThemeContext, ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 // Get the "root" div from index.html.
 // The React application will be inserted into this div.
