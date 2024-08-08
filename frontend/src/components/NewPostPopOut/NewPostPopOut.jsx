c6dccb05caa43ed827f3b050bef93f0bcba3bfa0
QS-Coding <169077428+QS-Coding@users.noreply.github.com>
2024-08-08 17:02:14 +0100

    Removed excess import

--- frontend/src/components/NewPostPopOut/NewPostPopOut.jsx
+++ frontend/src/components/NewPostPopOut/NewPostPopOut.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 import { createPost } from '../../services/posts';
