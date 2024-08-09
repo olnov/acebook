d8caf251c571018725ea0d67006eb72454c5266b
Oleg Novikov <o.novikov@ymail.com>
2024-08-09 14:11:46 +0100

    Fixed styles in profile page

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -88,6 +88,7 @@ const addRemoveFriend = async (req, res) => {
     res.status(200).json(user.friends);
   } catch (err) {
     res.status(500).json({ message: "Error adding/removing friend", error: err.message });
+    console.log(err)
   }
 };
 
